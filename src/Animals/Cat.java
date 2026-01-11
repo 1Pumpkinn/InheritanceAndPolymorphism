35f3fd63111a86e3df1a9080a67846bf34c7b7f8
Pumpkin <wishsmp1002@gmail.com>
2026-01-11 14:34:11 +1100

    Initial commit

---
+++ src/Animals/Cat.java
@@ -0,0 +1,17 @@
+public class Cat extends Animal{
+
+
+    public Cat(String picture, String name, int age) {
+        super(picture, name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(getName() + " meowed");
+    }
+
+    public void scratch() {
+        System.out.println(getName() + " scratched you");
+    }
+
+}
---
+++ src/Animals/Dog.java
@@ -0,0 +1,21 @@
+public class Dog extends Animal {
+
+    public static int numberOfDogs = 0;
+
+    public Dog(String picture, String name, int age) {
+        super(picture, name, age); // Basically the same as new Animal(...)
+
+        numberOfDogs++;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(getName() + " barked");
+    }
+
+    public void bellyRub() {
+        System.out.println("Belly rubbed of " + getName());
+
+    }
+
+}
